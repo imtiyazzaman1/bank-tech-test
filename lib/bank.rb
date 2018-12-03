8c27f60fc04cc1b5cc32abd9bbd52500d8af07c9
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-12-03 14:12:14 +0000

    integrate transaction class and removed redundant private method

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,3 +1,4 @@
+require_relative 'transaction'
 # This is the class the user would interact with. It stores the users balance
 class Bank
   attr_reader :balance
@@ -5,21 +6,21 @@ class Bank
     @balance = 0
   end
 
-  def deposit(amount)
-    add_to_balance(amount)
+  def deposit(amount, new_transaction = Transaction.new(@balance))
+    transaction = new_transaction
+    transaction.credit(amount)
+    new_balance(transaction.balance)
   end
 
-  def withdraw(amount)
-    subtract_from_balance(amount)
+  def withdraw(amount, new_transaction = Transaction.new(@balance))
+    transaction = new_transaction
+    transaction.debit(amount)
+    new_balance(transaction.balance)
   end
 
   private
 
-  def add_to_balance(amount)
-    @balance += amount
-  end
-
-  def subtract_from_balance(amount)
-    @balance -= amount
+  def new_balance(amount)
+    @balance = amount
   end
 end
