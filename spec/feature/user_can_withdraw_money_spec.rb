e64978fd2bd7151eec8d4df4bda853537006f7b2
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-12-03 13:43:59 +0000

    fixed error

--- spec/feature/user_can_withdraw_money_spec.rb
+++ spec/feature/user_can_withdraw_money_spec.rb
@@ -2,6 +2,7 @@ require_relative '../../lib/bank'
 
 describe('User can withdraw money') do
   it 'deducts money from the balance money is withdrawn' do
+    bank = Bank.new
     bank.deposit(1000)
     bank.withdraw(500)
     expect(bank.balance).to eq(500)
