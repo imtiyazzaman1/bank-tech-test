172967b15bff45d3c414b1ebcf5358630d2fbf07
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-12-03 15:38:48 +0000

    #print returns the information of each transaction in reverse order

--- lib/statement.rb
+++ lib/statement.rb
@@ -8,4 +8,37 @@ class Statement
   def add(transaction)
     @transactions << transaction
   end
+
+  def print
+    @transactions.reverse!
+
+    table = 'date || credit || debit || balance'
+
+    @transactions.each do |transaction|
+      table += "\n"
+      table += add_date(transaction)
+      table += add_credit(transaction)
+      table += add_debit(transaction)
+      table += transaction.balance
+    end
+    table
+  end
+
+  private
+
+  def add_date(transaction)
+    "#{transaction.date} || "
+  end
+
+  def add_credit(transaction)
+    return "#{transaction.amount} || " if transaction.type == 'credit'
+
+    '|| '
+  end
+
+  def add_debit(transaction)
+    return "#{transaction.amount} || " if transaction.type == 'debit'
+
+    '|| '
+  end
 end
--- spec/units/statement_spec.rb
+++ spec/units/statement_spec.rb
@@ -1,11 +1,31 @@
 require 'statement'
 
 describe Statement do
-  let(:transaction) { double :transaction }
+  let(:transaction_1) { double :transaction_1 }
+  let(:transaction_2) { double :transaction_2 }
   describe '#add' do
     it 'stores transaction' do
-      subject.add(transaction)
-      expect(subject.transactions).to include transaction
+      subject.add(transaction_1)
+      expect(subject.transactions).to include transaction_1
+    end
+  end
+
+  describe '#print' do
+    it 'displays the stored transactions' do
+      subject.add(transaction_1)
+      allow(transaction_1).to receive(:date).and_return('03/12/2018')
+      allow(transaction_1).to receive(:type).and_return('credit')
+      allow(transaction_1).to receive(:balance).and_return('1000.00')
+      allow(transaction_1).to receive(:amount).and_return('1000.00')
+
+      subject.add(transaction_2)
+      allow(transaction_2).to receive(:date).and_return('03/12/2018')
+      allow(transaction_2).to receive(:type).and_return('debit')
+      allow(transaction_2).to receive(:balance).and_return('700.00')
+      allow(transaction_2).to receive(:amount).and_return('300.00')
+
+      expect(subject.print).to eq "date || credit || debit || balance
+03/12/2018 || || 300.00 || 700.00\n03/12/2018 || 1000.00 || || 1000.00"
     end
   end
 end
