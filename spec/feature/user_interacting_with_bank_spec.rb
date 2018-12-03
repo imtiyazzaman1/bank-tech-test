1cbb1c736f40a9ab0799b3346c3f838d40ffbab7
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-12-03 11:31:10 +0000

    renamed feature test

--- spec/feature/user_interacting_with_bank_spec.rb
+++ spec/feature/user_interacting_with_bank_spec.rb
@@ -1,5 +1,5 @@
 require_relative '../../lib/bank'
-describe('User can deposit money') do
+describe('User interacting with bank') do
   it('shows balance of 1000 when 1000 has been deposited') do
     bank = Bank.new
     bank.deposit(1000)
