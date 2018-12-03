35dad6ed56d89f3d013c9bb14f14a6dd2cc094ab
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-12-03 14:12:26 +0000

    used doubles to stub dependancy on Transaction

--- spec/units/bank_spec.rb
+++ spec/units/bank_spec.rb
@@ -1,17 +1,26 @@
 require 'bank'
 
 describe Bank do
+  let(:transaction) { double :transaction }
+
+  before(:each) do
+    allow(transaction).to receive(:credit).with(1000)
+    allow(transaction).to receive(:balance).and_return(1000)
+    subject.deposit(1000, transaction)
+  end
+
   describe('#deposit') do
     it 'stores the deposited amount' do
-      subject.deposit(1000)
       expect(subject.balance).to eq(1000)
     end
   end
 
   describe('#withdraw') do
     it 'subtracts money from the current balance' do
-      subject.deposit(1000)
-      subject.withdraw(500)
+      allow(transaction).to receive(:debit).with(500)
+      allow(transaction).to receive(:balance).and_return(500)
+
+      subject.withdraw(500, transaction)
       expect(subject.balance).to eq(500)
     end
   end
