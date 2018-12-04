28a58cc2adb4690b59f816cd6f8133d36861cfa1
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-12-04 11:01:30 +0000

    removed timecop

--- spec/units/transaction_spec.rb
+++ spec/units/transaction_spec.rb
@@ -3,16 +3,12 @@ require 'timecop'
 
 describe(Transaction) do
   let(:subject) { described_class.new(1000) }
-  before do
-    time = Time.local(2018, 12, 3, 0, 0)
-    Timecop.freeze(time)
-  end
-
-  after do
-    Timecop.return
-  end
 
   it 'has the date of when it occurs' do
     expect(subject.date).to eq('03/12/2018')
+  end
+
+  it 'has a balance' do
+    expect(subject.balance).to eq 1000
   end
 end
